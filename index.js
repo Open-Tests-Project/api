52268df9463287ee9bfe8807f38b2a9a6a79e35f
Ubuntu <ubuntu@ip-172-31-19-220.eu-west-3.compute.internal>
2020-05-13 16:03:10 +0000

    add cv/visitcounter route

--- index.js
+++ index.js
@@ -3,5 +3,33 @@
 
 var path = require("path");
 var config = require(path.resolve(process.cwd(), "config"));
+var redis =  require(path.resolve(process.cwd(), "redis"));
 
-console.log(config);
+
+const fastify = require('fastify')({
+    logger: false
+});
+  
+// http://zsimo.it/api/cv/visitcounter
+fastify.get('/cv/visitcounter', function (request, reply) {
+    redis.incr("cv/visitcounter", function (err, res) {
+
+        if (err) {
+            reply.code(400).send({'Bad request': ""})
+        } else {
+            reply.send({ visitcounter: res });
+        }
+
+    });
+    
+});
+
+// Run the server!
+fastify.listen(3000, function (err, address) {
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+    // fastify.log.info(`server listening on ${address}`)
+    console.log(`server listening on ${address}`);
+});
